d717eb3990d9240ae9ac208a3a40acb69c26723e
damiatorres <damia.torres@gmail.com>
2017-02-11 16:21:55 +0100

    Add files via upload

--- imagetotable.py
+++ imagetotable.py
@@ -1,13 +1,16 @@
 from PIL import Image
 import PIL
-img = Image.open('image.png').convert('LA')
-img = img.resize((33, 33), PIL.Image.ANTIALIAS)
-inf = 256.0
-sup = -1.0
-for pixel in iter(img.getdata()):
-    inf = min(pixel[0], inf)
-    sup = max(pixel[0], sup)
-for pixel in iter(img.getdata()):
-    tmp = 0.0 + pixel[0]
-    tmp = 256 * (tmp - inf) / (sup - inf)
-    print(tmp)
+for n in range(1, 3):
+  img = Image.open(str(n) + '.png').convert('LA')
+  img = img.resize((10, 10), PIL.Image.ANTIALIAS)
+  inf = 256.0
+  sup = -1.0
+  for pixel in iter(img.getdata()):
+      inf = min(pixel[0], inf)
+      sup = max(pixel[0], sup)
+  f=open('./input' + str(n) + '.txt', 'w')
+  for pixel in iter(img.getdata()):
+      tmp = 0.0 + pixel[0]
+      tmp = 256 * (tmp - inf) / (sup - inf)
+      f.write(str(tmp) + ' '),
+  f.close()
